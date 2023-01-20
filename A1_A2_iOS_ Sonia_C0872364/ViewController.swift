1d00d331f30b1f6973f7106b5bbad666496da475
Sonia364 <c0872364@mylambton.ca>
2023-01-20 17:22:58 -0500

    Displayed the location of the user on the map with a proper zooming.

--- A1_A2_iOS_ Sonia_C0872364/ViewController.swift
+++ A1_A2_iOS_ Sonia_C0872364/ViewController.swift
@@ -6,12 +6,97 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // map zooming
+        map.isZoomEnabled = true
+        map.showsUserLocation = true
+        map.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    // location manager
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        displayLocation(latitude: latitude, longitude: longitude, title: "User Location", subtitle: "you are here")
+        
+    }
+    
+    func displayLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, title: String, subtitle: String) {
+        // 2 - define delta latitude and delta longitude for the span
+        let latDelta: CLLocationDegrees = 0.05
+        let lngDelta: CLLocationDegrees = 0.05
+        
+        // 3 - creating the span and location coordinate and finally the region
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: location, span: span)
+        let loc: CLLocation = CLLocation(latitude: latitude, longitude: longitude)
+        // 4 - set region for the map
+        map.setRegion(region, animated: true)
+        let annotation = MKPointAnnotation()
+        annotation.title = title
+        
+        CLGeocoder().reverseGeocodeLocation(loc) { (placemarks, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                if let placemark = placemarks?[0] {
+                    
+                    annotation.title = placemark.subThoroughfare! + " " + placemark.thoroughfare! + ", " + placemark.locality! + ", " + placemark.country!
+                }
+            }
+        }
+        
+        annotation.coordinate = location
+        map.addAnnotation(annotation)
+        
+    }
+    
+    //MARK: - viewFor annotation method
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+//        if annotation is MKUserLocation {
+//            return nil
+//        }
+        
+        switch annotation.title {
+        case "User Location":
+            let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "MyMarker")
+            annotationView.pinTintColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+            annotationView.canShowCallout = true
+            return annotationView
+        case "my destination":
+            let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+            annotationView.animatesDrop = true
+            annotationView.pinTintColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+            return annotationView
+        case "my favorite":
+            let annotationView = map.dequeueReusableAnnotationView(withIdentifier: "customPin") ?? MKPinAnnotationView()
+            annotationView.image = UIImage(named: "ic_place_2x")
+            annotationView.canShowCallout = true
+            annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            return annotationView
+        default:
+            return nil
+        }
     }
 
 
