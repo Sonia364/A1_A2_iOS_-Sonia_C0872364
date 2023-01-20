267b1bdc889aa0f34b045febbd685b5cf03437d2
Sonia364 <c0872364@mylambton.ca>
2023-01-20 16:54:22 -0500

    Initial Commit

---
+++ A1_A2_iOS_ Sonia_C0872364/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_ Sonia_C0872364
+//
+//  Created by Sonia Nain on 2023-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
